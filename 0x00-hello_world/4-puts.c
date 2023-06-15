eabf92e7e095210ce12d60575ed7bac738d1d702
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-15 10:02:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ *main-is the entry point
+ *return- when it is 0 mean successful otherwise error occured
+ */
+int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-	return 0
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 

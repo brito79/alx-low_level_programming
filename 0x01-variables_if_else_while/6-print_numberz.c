3a92a4946e2b9bcfe9953383f7ad91aff2bdf82a
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-17 11:53:24 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main- Entry point
+ *Return: Always 0 (indicating success)
+ *Description-testing positive,negative or zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}

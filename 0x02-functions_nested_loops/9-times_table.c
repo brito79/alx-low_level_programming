da787bc94a0f2a4a1e967f912adc18a8a1fe46a0
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-21 11:00:24 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,48 @@
 #include "main.h"
 /**
- *main- Entry point
- *Return: Always 0 (indicating success)
- *Description-testing positive,negative or zero
- */
+  * times_table - Constructing time table, starting with 0
+  * _putchar - Prints numbers
+  */
+
 void times_table(void)
 {
 	int s = 0, e = 9, count = 0;
+
 	while (count <= 9)
 	{
 		while (s <= e)
 		{
-			int product = count * s
-				if (s != 0)
-				{
-					_putchar(',');
-					if (product < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else
-						_putchar(' ');
-				}
-			if (product < 10)
-				_putchar(product + '0');
-			else
+			int product = count * s;
+
+			if (s != 0)
 			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
+				_putchar(',');
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				if (product < 10)
+				{
+					_putchar(product + '0');
+				}
+				else
+				{
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				if (s == e)
+				{
+					_putchar('\n');
+				}
+				s++;
 			}
-			if (s ==e)
-				_putchar('\n');
-			s++;
+			s = 0;
+			count++;
 		}
-		s = 0;
-		count++;
 	}
 }

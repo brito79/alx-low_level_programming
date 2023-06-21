f93bd7711b81fcf0ce73f47280198f31f51c43b1
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-21 06:02:48 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- *main- Entry point
- *Return: Always 0 (indicating success)
- *Description-testing positive,negative or zero
- */
+  * _isalpha - Checks for alphabet character
+  * @c: The character to be checked
+  *
+  *Return: 1 when c is lower or uppercase, otherwise 0
+  */
 int _isalpha(int c)
 {
-	if(c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
-	esle
+	else
 	{
 		return (0);
 	}

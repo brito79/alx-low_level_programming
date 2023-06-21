27d5e075d9f2fb6a4d7be3256c1c020cc7e2d1be
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-21 14:21:49 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,31 +6,20 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n <= 98)
+	while (n != 98)
 	{
-		for (i = n; i < 98; i++)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	else
-	{
-		for (i = n; i > 98; i--)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
+			n--;
+		else
+			n++;
 	}
 
-	_putchar('9');
-	_putchar('8');
+	_putchar((n / 10) + '0');
+	_putchar((n % 10) + '0');
 	_putchar('\n');
 }
 

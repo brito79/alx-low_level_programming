287a338137d4e8b28b01a4e3f798210af0668e6a
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-21 06:59:11 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,25 @@
 #include "main.h"
+
 /**
- *main- Entry point
- *Return: Always 0 (indicating success)
- *Description-testing positive,negative or zero
+ * print_last_digit - Prints the last digit
+ * @num1: The number to be checked
+ *
+ * Return: -last when negative, otherwise positive
  */
 int print_last_digit(int num1)
 {
 	int last;
 
-	last = num1%10;
-	if (last < 0);
+	last = num1 % 10;
+	if (last < 0)
 	{
-		_putchar(-last + 48);
-		return(-last);
+		_putchar(-last + '0');
+		return (-last);
 	}
 	else
 	{
-		_putchar(last + 48);
-		return(last);
+		_putchar(last + '0');
+		return (last);
 	}
 }
 

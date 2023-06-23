a79537202a1804435786faa554cbb8ecceec8725
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-23 05:25:58 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 
 /**
-* print_fibonacci_sequence - Prints the Fibonacci  given number.
+* print_fibonacci_sequence - Prints the Fibonacci sequence  number
+* and calculates the sum of the even-valued terms.
 * @n: The number up to which the Fibonacci sequence is printed.
 *
-* The function calculates and prints the Fibonacci sequence given number.
+* The function calculates and prints the Fibonacci  number.
+* It also calculates the sum of the even-valued terms.
 * The numbers are separated by commas and a space.
 */
 void print_fibonacci_sequence(unsigned long n)
 {
 unsigned long prev = 1, curr = 2;
 unsigned long fib;
+unsigned long sum = 2;
 
 printf("%lu, %lu", prev, curr);
 
 while (curr <= n)
 {
 fib = prev + curr;
+
+if (fib % 2 == 0)
+{
+sum += fib;
+}
+
 printf(", %lu", fib);
 
 prev = curr;
 curr = fib;
 }
 
-printf("\n");
+printf("\nSum of even-valued terms: %lu\n", sum);
 }
 
 /**
 * main - Entry point of the program.
 *
-* Calls the print_fibonacci the value 4000000
-* to print the Fibonacci sequence up to 4,000,000.
+* Calls the print_fibonacci_sequence  the value 4000000
+* to print the Fibonacci sequence up to 4,000,000
 *
 * Return: 0 on success.
 */

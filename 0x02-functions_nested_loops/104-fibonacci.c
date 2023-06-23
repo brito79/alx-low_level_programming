458773415196089bc51de7c405b475d5821d85e5
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-23 05:55:07 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,52 +1,50 @@
 #include <stdio.h>
 
 /**
-* print_fibonacci_sequence - Prints the Fibonacci sequence  number
-* and calculates the sum of the even-valued terms.
-* @n: The number up to which the Fibonacci sequence is printed.
-*
-* The function calculates and prints the Fibonacci  number.
-* It also calculates the sum of the even-valued terms.
-* The numbers are separated by commas and a space.
-*/
+ * print_fibonacci_sequence - Prints Fibonacci sequence
+ * @n: Number up to which Fibonacci sequence is printed.
+ *
+ * Calculates and prints Fibonacci sequence up to given number.
+ * Also calculates sum of even-valued terms.
+ * Numbers are separated by commas and a space.
+ */
 void print_fibonacci_sequence(unsigned long n)
 {
-unsigned long prev = 1, curr = 2;
-unsigned long fib;
-unsigned long sum = 2;
+	unsigned long prev = 1, curr = 2;
+	unsigned long fib;
+	unsigned long sum = 2; /* Starting with initial even term (2) */
 
-printf("%lu, %lu", prev, curr);
+	printf("%lu, %lu", prev, curr);
 
-while (curr <= n)
-{
-fib = prev + curr;
+	while (curr <= n)
+	{
+		fib = prev + curr;
 
-if (fib % 2 == 0)
-{
-sum += fib;
-}
+		if (fib % 2 == 0)
+			sum += fib;
 
-printf(", %lu", fib);
+		printf(", %lu", fib);
 
-prev = curr;
-curr = fib;
-}
+		prev = curr;
+		curr = fib;
+	}
 
-printf("\nSum of even-valued terms: %lu\n", sum);
+	printf("\nSum of even-valued terms: %lu\n", sum);
 }
 
 /**
-* main - Entry point of the program.
-*
-* Calls the print_fibonacci_sequence  the value 4000000
-* to print the Fibonacci sequence up to 4,000,000
-*
-* Return: 0 on success.
-*/
+ * main - Entry point of program.
+ *
+ * Calls print_fibonacci_sequence function with value 4000000
+ * to print Fibonacci sequence up to 4,000,000 and calculate
+ * sum of even-valued terms.
+ *
+ * Return: 0 on success.
+ */
 int main(void)
 {
-print_fibonacci_sequence(4000000);
+	print_fibonacci_sequence(4000000);
 
-return (0);
+	return (0);
 }
 

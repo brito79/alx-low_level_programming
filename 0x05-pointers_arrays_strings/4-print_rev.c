ebaf6f2204228ff9cdab8eaa5ddf9f66653c2a1f
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-26 12:27:09 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_rev - Prints the characters of a string in reverse order.
+* @s: The string to be printed in reverse.
+*/
+void print_rev(char *s)
+{
+int length = 0;
+int i;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+s--;
+
+for (i = length; i > 0; i--)
+{
+_putchar(*s);
+s--;
+}
+
+_putchar('\n');
+}
+

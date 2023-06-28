fa1d24351fb76898c8332fde462faa9899405184
Brian Shayamano <bshayamano2002@gmail.com>
2023-06-28 03:15:13 -0700

    changed code

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,12 +13,12 @@ long int fibn = 0;
 int i;
 int ln = 50;
 
-printf("%lld, %lld", term1, term2);
+printf("%ld, %ld", term1, term2);
 
 for (i = 3; i <= ln; i++)
 {
 fibn = term1 + term2;
-printf(", %lld", fibn);
+printf(", %ld", fibn);
 term1 = term2;
 term2 = fibn;
 }

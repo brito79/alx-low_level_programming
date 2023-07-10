37d8ba8e6c2b7f47304bfd7bc3d7c5476c667a60
Brian Shayamano <bshayamano2002@gmail.com>
2023-07-10 07:49:53 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 
-	char* s = malloc(size * sizeof(unsigned int));
+	char* s = malloc(size * sizeof(char + 1));
 
 	while (i < size)
 	{

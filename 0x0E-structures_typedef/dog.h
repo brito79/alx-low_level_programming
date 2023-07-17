c09aa0f64e9da933ab78aaacb18732bc51fe7854
Brian Shayamano <bshayamano2002@gmail.com>
2023-07-17 11:54:11 -0700

    I am done with struct and type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 /**
   * struct dog - A structure representing dog
-  * @name: Name of a dog
+i  * @name: Name of a dog
   * @age: How old a dog is
   * @owner: Dog's onwer
   */
@@ -12,5 +12,18 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 
 #endif

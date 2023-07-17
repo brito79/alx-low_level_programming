30e8f9e584329e9582136b94226c5a7d16784565
Brian Shayamano <bshayamano2002@gmail.com>
2023-07-17 10:22:57 -0700

    I am done,with preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Description - The program that prints use __FILE__
+  * Return: 0 on success
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef FUCTION_LIKE_MACRO_H
+#define FUCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
+

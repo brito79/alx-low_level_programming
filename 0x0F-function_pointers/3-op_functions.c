84d5354f69f389d1590594aa2a9b7fe48d88fdbe
Brian Shayamano <bshayamano2002@gmail.com>
2023-07-19 12:27:38 -0700

    Question3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,61 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
+/**
+  * op_add - Adding numbers
+  * @a: The first number
+  * @b: The second number
+  * Return: Operated result interger
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - subtracting numbers
+  * @a: The first number
+  * @b: The second number
+  * Return: Operated result interger
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * op_mul - multiplying numbers
+  * @a: The first number
+  * @b: The second number
+  * Return: Operated result interger
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - Dividing numbers
+  * @a: The first number
+  * @b: The second number
+  * Return: Operated result interger
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - modulo of a  number
+  * @a: The first number
+  * @b: The second number
+  * Return: Operated result interger
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

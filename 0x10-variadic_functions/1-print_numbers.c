14bff2d7b54e8fde1213e3998c52603b9c65abed
Brian Shayamano <bshayamano2002@gmail.com>
2023-07-20 05:57:26 -0700

    I am done

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,39 +1,36 @@
 #include "variadic_functions.h"
+
 /**
- * print_numbers - printing of integers separated by a separator
- * @n: The number of variadic arguments passed
- * @separator: The charactor then will separate integers
- * @...: The ellipsis, mean variable arguments to be taken
- * Return: The void
- */
+* print_numbers - Print integers separated by a separator.
+* @separator: The character that separates the integers (or NULL).
+* @n: The number of integers to be printed.
+* @...: The ellipsis, representing the variable arguments to be printed.
+*/
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	int num = 0;
+unsigned int i;
+int num = 0;
 
-	va_list ap;
+va_list ap;
+va_start(ap, n);
 
-	va_start(ap, n);
-
-	if (n == 0)
-	{
-		return;
-	}
-	for (i = 0; i < n; i++)
-	{
-		num = va_arg(ap, int);
-		printf("%d", num);
-
-		if (i < n - 1 && separator != NULL)
-		{
-			printf("%s", separator);
-		}
-		else if(separator == NULL)
-		{
-			return;
-		}
-		
-	}
-	printf("\n");
-	va_end(ap);
+if (n == 0)
+{
+return;
 }
+
+for (i = 0; i < n; i++)
+{
+num = va_arg(ap, int);
+printf("%d", num);
+
+if (separator != NULL && i < n - 1)
+{
+printf("%s", separator);
+}
+}
+
+printf("\n");
+va_end(ap);
+}
+

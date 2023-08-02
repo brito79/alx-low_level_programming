7702c27b3777d8307ae87b3c2af4d096e28f0e67
Brian Shayamano <bshayamano2002@gmail.com>
2023-08-02 05:47:20 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,30 +6,19 @@
  *
  * Return: address of the node where the loop starts, or NULL
  */
-listint_t *find_listint_loop(listint_t *head)
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *slow = head;
-	listint_t *fast = head;
+	listint_t *nextnode = NULL;
+	listint_t *prevnode = NULL;
 
-	if (!head)
-		return (NULL);
-
-	while (slow && fast && fast->next)
+	while (*head)
 	{
-		fast = fast->next->next;
-		slow = slow->next;
-		if (fast == slow)
-		{
-			slow = head;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-			}
-			return (fast);
-		}
+		nextnode = (*head)->next;
+		(*head)->next = prevnode;
+		prevnode = *head;
+		*head = nextnode;
 	}
 
-	return (NULL);
+	*head = prevnode;
+	return (*head);
 }
-

d66f9c85dced4126d55f32019564fa2ec3b25c71
Brian Shayamano <bshayamano2002@gmail.com>
2023-08-04 11:36:46 -0700

    Bit Manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * get_endianness - Checks the endianness of a number
+  * Return: The value Endianness
+  */
+int get_endianness(void)
+{
+	unsigned int val = 1;
+
+	char *ch = (char *) & val;
+	return (*ch);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+int get_endianness(void);
 
 #endif

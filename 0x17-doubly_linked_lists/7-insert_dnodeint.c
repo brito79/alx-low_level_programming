de350cf82193ca1d9588487098328fab78e09f22
Brito79 <bshayamano2002@gmail.com>
2024-01-26 09:04:55 -0800

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,34 +6,56 @@
 * @n: Value to be inserted
 * Return: Pointer to new inserted node
 */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *temp = NULL;
 unsigned int i = 0;
 dlistint_t *new_node;
 
-if (*h == NULL)
+if (*h == NULL && idx > 0)
 {
 return (NULL);
 }
-temp = *h;
 
-while (i < idx - 1)
-{
-temp = temp->next;
-i++;
-}
 new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-
 if (new_node == NULL)
 {
 return (NULL);
 }
 new_node->n = n;
-temp->next->prev = new_node;
+
+if (idx == 0)
+{
+new_node->next = *h;
+new_node->prev = NULL;
+if (*h != NULL)
+{
+(*h)->prev = new_node;
+}
+*h = new_node;
+return (new_node);
+}
+
+temp = *h;
+
+while (i < idx - 1)
+{
+temp = temp->next;
+if (temp == NULL)
+{
+free(new_node);
+return (NULL);
+}
+i++;
+}
+
 new_node->next = temp->next;
-temp->next = new_node;
 new_node->prev = temp;
+if (temp->next != NULL)
+{
+temp->next->prev = new_node;
+}
+temp->next = new_node;
+
 return (new_node);
 }

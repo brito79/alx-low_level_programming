789bded3679b7f876c97234e4c12b4c5bee3978a
Brito79 <bshayamano2002@gmail.com>
2024-02-02 11:21:49 -0800

    1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - The Algorthm
+ * @str: The string to iterate in
+ * Return: The calculated value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 1581;
+
+	while (c = *str++)
+	{
+		hash += ((hash << 5) + hash) + c /*hash * 33 + c */
+	}
+	return (hash);
+}

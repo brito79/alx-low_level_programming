7ae316a175bd1b0322f04a8e044f7fc904fd88ef
Brito79 <bshayamano2002@gmail.com>
2024-02-02 10:45:55 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,25 +7,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i = 0;
+	unsigned long int i;
 
-	hash_table_t *ht = (hash_table_t *)malloc(sizeof(hash_table_t));
-
-
+	hash_table_t *ht;
+	
+	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+	
 	if (ht == NULL)
 	{
 		return (NULL);
 	}
-
 	ht->size = size;
-
 	ht->array = (hash_node_t **)calloc(ht->size, sizeof(hash_node_t *) * size);
-
+	
 	if (ht->array == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		ht->array[i] = NULL;

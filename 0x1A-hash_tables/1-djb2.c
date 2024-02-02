97c75f7d8a7691e188acf5129a0f8dc1449f414a
Brito79 <bshayamano2002@gmail.com>
2024-02-02 11:25:30 -0800

    1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -13,7 +13,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	while (c = *str++)
 	{
-		hash += ((hash << 5) + hash) + c /*hash * 33 + c */
+		hash = ((hash << 5) + hash) + c; /*hash * 33 + c */
 	}
 	return (hash);
 }
